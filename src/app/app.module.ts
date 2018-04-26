870385d5ac0934a9404a49cded2ba54546b0adb1
Joanna Piecuch <joanna.koterba92@gmail.com>
2018-04-26 12:04:39 +0200

    Create panel component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { PanelComponent } from './main/panel/panel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
